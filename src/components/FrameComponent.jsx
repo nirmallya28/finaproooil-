41c8bd866233e452d315942ea5f061c4263b8f61
nirmallya28 <157309613+nirmallya28@users.noreply.github.com>
2024-07-23 21:54:19 +0530

    Update FrameComponent.jsx

--- src/components/FrameComponent.jsx
+++ src/components/FrameComponent.jsx
@@ -1,12 +1,18 @@
+import React from "react";
 import GroupComponent from "./GroupComponent";
 import PropTypes from "prop-types";
+
+// Global variable to store the number of GroupComponent instances
+const numberOfGroups = 0;
 
 const FrameComponent = ({ className = "" }) => {
   return (
     <section
       className={`self-stretch flex flex-row items-start justify-end pt-0 pb-[11px] pr-[26px] pl-[30px] box-border max-w-full text-left text-17xl text-black font-inter ${className}`}
     >
-      <GroupComponent />
+      {Array.from({ length: numberOfGroups }).map((_, index) => (
+        <GroupComponent key={index} />
+      ))}
     </section>
   );
 };
